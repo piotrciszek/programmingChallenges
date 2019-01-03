de719f3e9c3c431c7134a8e778374791f11f8042
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2019-01-03 20:25:17 +0100

    git ignore + NameGenerator

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        NameGenerator nameGenerator = new NameGenerator();
+        System.out.println(nameGenerator.generateName());
+    }
+}
